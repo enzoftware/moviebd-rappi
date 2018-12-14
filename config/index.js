b33794206b2e9d59bb5ca834cab8179bf2322a05
enzoftware <lizama.enzo@gmail.com>
2018-12-14 09:55:14 -0500

    Hello world :wave: Multilayer architecture rest api

---
+++ config/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    port : 3000,
+    dbConnectionString : 'CONNECTION_URL',
+    saltRounds : 2,
+    jwtSecret : 'secret',
+    tokenExpireTime : '6h'
+}
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const app = express();
+const config = require('./config/index');
+const router = require('./router');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended : true
+}));
+app.use(express.static('client'));
+router.set(app);
+app.listen(config.port, () => console.log("App listening on port " + config.port));
