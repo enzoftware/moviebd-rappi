e0da16780961a65d48a8adec6ca8f742408be008
enzoftware <lizama.enzo@gmail.com>
2018-12-16 23:23:36 -0500

    order controller

--- router.js
+++ router.js
@@ -1,6 +1,10 @@
 const authController = require('./controllers/auth');
+const orderController = require('./controllers/order');
 
 module.exports.set = app => {
     app.post('/login', authController.login);
     app.post('/register', authController.register);
+    app.get('/orders',orderController.getOrders);
+    app.get('/order/:id', orderController.getOrder);
+    app.post('/orders', orderController.addOrder);
 }
